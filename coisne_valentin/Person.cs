a5c9d58d9745834a2e2e1ac77caacda810376462
Seth-77 <coisne.valentin92@gmail.com>
2025-10-11 12:43:47 +0200

    Creation Person Class

---
+++ coisne_valentin/Person.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Person
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required DateTime BirthDate { get; set; }
+}
--- coisne_valentin/Program.cs
+++ coisne_valentin/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            BirthDate = new DateTime(1990, 1, 1)
+        };
+
+        Console.WriteLine($"Name: {person.FirstName} {person.LastName}, Birth Date: {person.BirthDate.ToShortDateString()}");
+    }
+}   
